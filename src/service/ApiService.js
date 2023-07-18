13d4f31d48bd6092a8c7044b7b2e4e00091ed7bf
handic-public <parkjbh@naver.com>
2023-07-18 19:55:54 +0900

    로그인페이지만드는중

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -13,12 +13,31 @@ export function call(api, method, request) {
         // GET method
         options.body = JSON.stringify(request);
     }
-    return fetch(options.url, options).then((response) => {
-        if(response.status === 200) {
-            return response.json();
-        }
-    }).catch((error) => {
-        console.log("http error");
-        console.log(error);
-    });
+
+    return fetch(options.url, options)
+        .then((response) =>
+            response.json().then((json) => {
+                if( !response.ok) {
+                    // response.ok가 true이면 정상적인 응답을 받은 것이고 아니면 에러 응답을 받은건
+                    return Promise.reject(json);
+                }
+                return json;
+            })
+        )
+        .catch((error) => {
+            // 추가 된 부분
+            console.log(error.status);
+            if(error.status === 403) {
+                window.location.href = "/login";
+            }
+            return Promise.reject(error);
+        });
+    // return fetch(options.url, options).then((response) => {
+    //     if(response.status === 200) {
+    //         return response.json();
+    //     }
+    // }).catch((error) => {
+    //     console.log("http error");
+    //     console.log(error);
+    // });
 }
