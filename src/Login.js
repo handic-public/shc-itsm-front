310ab11dca0d4cd5ba2383da4a7c450b49c2ed60
handic-public <parkjbh@naver.com>
2023-07-21 11:29:58 +0900

    로그인까지 완성(로컬스토리지사용)

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {signin} from "./service/ApiService";
-import Button from "@material-ui/core/Button";
-import Textfield from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
-import { Typography } from "@material-ui/core/Typography";
+//import Button from "@material-ui/core/Button";
+//import Textfield from "@material-ui/core/TextField";
+//import Grid from "@material-ui/core/Grid";
+// import { Typography } from "@mui/material/Typography";
 import { Container } from "@material-ui/core/";
+
+import { Button, Grid, TextField, Typography } from "@mui/material";
 
 class Login extends React.Component {
     constructor(props) {
@@ -22,7 +24,35 @@ class Login extends React.Component {
     }
 
     render() {
-        return <p>로그인 페이지</p>
+        return (
+            <Container componse="main" maxWidth="xs" style={{ marginTop: "8%"}}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Typography component="h1" variant="h5">
+                            로그인
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <form noValidate onSubmit={this.handleSubmit}>
+                    {" "}
+                    {/* submit 버튼을 클릭하면 handleSubmit이 실행됨 */}
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField variant="outlined" required fullWidth id="username" label="아이디" name="username" /*autoComplete="email"*/ />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField varinat="outlined" required fullWidth id="password" label="패스워드" name="password" type="password" autoComplete="current-password" />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button type="submit" fullWidth variant="contained" color="primary">
+                                로그인
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+                <p></p>
+            </Container>
+        );
     }
 }
 
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,10 +1,19 @@
 import { API_BASE_URL } from "../api-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request) {
+    let headers = new Headers({
+        "Content-Type" : "application/json",
+    });
+
+    // 로컬 스토리지에서 ACESS TOKEN 가져오기
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if(accessToken && accessToken !== null) {
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+    
     let options = {
-        headers: new Headers({
-            "Content-Type" : "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -13,20 +22,31 @@ export function call(api, method, request) {
         // GET method
         options.body = JSON.stringify(request);
     }
-
+    
     return fetch(options.url, options)
-        .then((response) => 
-            response.json().then((json) => {
-                // response.ok가 true이면 정상적인 응답을 받은 것이고 아니면 에러 응답을 받은것
-                if(!response.ok) {
-                    return Promise.reject(json);
-                }
-                return json;
-            })
-        )
+        .then((response) => {
+            // console.log(response.status);
+            if(response.status === 403) {
+                window.location.href = "/login";    // redirect
+                return;
+            }
+            // response.ok가 true이면 정상적인 응답을 받은 것이고 아니면 에러 응답을 받은것
+            if(!response.ok) {
+                return Promise.reject(response);
+            }
+            return response.json();
+        })
+        // 기존에는 이렇게 처리 했는데 'response.json()' 값이 promise여서 body부분 처리가 되질 않아 오류발생됨.
+        //     response.json().then((json) => {
+        //         if(!response.ok) {
+        //             return Promise.reject(json);
+        //         }
+        //         return json;
+        //     })
+        // )
         .catch((error) => {
             // 추가 된 부분
-            console.log(error.status);
+            console.log(error);
             if(error.status === 403) {
                 window.location.href = "/login";    // redirect
             }
@@ -47,5 +67,12 @@ export function signin(userDTO) {
     .then( (response) => {
         console.log("response : ", response);
         alert("로그인 토큰: " + response.token);
+        if(response.token) {
+            // 로컬스토리지에 토큰 저장
+            localStorage.setItem("ACCESS_TOKEN", response.token);
+            // token이 존재할경우 Tdo 화면으로 rediret
+            window.location.href = "/";
+        }
+
     });
 }
